ca721800bbfde29baf6f160a8044adcdd2c50b07
wanuch <wanuch.int@gmail.com>
2022-10-23 23:55:29 +0700

    update props

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,23 +1,26 @@
 
 import { Link } from 'react-router-dom';
+import classes from "./MainNavigation.module.css"
 
 function MainNavigation() {
-    return <header>
-        <div> React Meetups</div>
-        <nav>
-            <ul>
-                <li>
-                    <Link to='/'>All Meetups</Link>
-                </li>
-                <li>
-                    <Link to='/new-meetup'>Add New Meetup</Link>
-                </li>
-                <li>
-                    <Link to='/favorites'>My Favorites</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    return (
+        <header className={classes.header}>
+            <div> React Meetups</div>
+            <nav className={classes.logo}>
+                <ul>
+                    <li>
+                        <Link to='/'>All Meetups</Link>
+                    </li>
+                    <li>
+                        <Link to='/new-meetup'>Add New Meetup</Link>
+                    </li>
+                    <li>
+                        <Link to='/favorites'>My Favorites</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
 
 export default MainNavigation;
