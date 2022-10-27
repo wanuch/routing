73b16eec6abbbf5174cabecfff0fb01884eb5181
wanuch <wanuch.int@gmail.com>
2022-10-27 22:16:30 +0700

    add firebase, useState, and useEffect

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,28 @@
+import { useHistory } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupsPage() {
-    return <div>NewMeetupsPage</div>
+    // For Navigating
+    const history = useHistory();
+
+    function addMeetupHandler(meetupData) {
+        fetch("https://react-getting-start-62baa-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
+            {
+                method: "POST",
+                body: JSON.stringify(meetupData),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        ).then(() => {
+            history.replace('/');
+        });
+    }
+
+    return <section>
+        <h1>Add New Meetup</h1>
+        <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
 
 }
 
